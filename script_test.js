db4b509f8e8f06c213c35acd2a2edb13f29f4878
Sam K <retaildiamond@gmail.com>
2021-03-04 11:14:33 -0500

    changed test skill

--- script_test.js
+++ script_test.js
@@ -5,7 +5,7 @@ var urlParams = new URLSearchParams(window.location.search);
 var skillId = parseInt(urlParams.get('skill'));
 
 if(urlParams.get('skill') == 'test'){
-  skillId = 2688035330;
+  skillId = 3087411630;
 }
 
 // skill ID for the run through skill ID
